c94e0b19c26c17051fbb94650a8eae692f0f76e9
Micky Hulse <micky@registerguard.com>
2013-02-25 00:21:49 -0800

    A few bug fixes.

--- build/files/js/woof.forecast.js
+++ build/files/js/woof.forecast.js
@@ -39,15 +39,22 @@ WOOF.register(function() {
 		function successful(json) {
 			
 			// CSS media queries and style info for `<head>` of document:
+			
+			// http://stackoverflow.com/a/9050485/922323
+			$('head').append('<style type="text/css">@media only screen and (min-width: 1005px) { #forecast { background-repeat: no-repeat; background-image: url(' + assetFQDN + '/static/weather/' + json.code + '.png); } } @media only screen and (min-width: 1005px) and (min--moz-device-pixel-ratio: 1.5), only screen and (min-width: 1005px) and (-moz-min-device-pixel-ratio: 1.5), only screen and (min-width: 1005px) and (-o-min-device-pixel-ratio: 3/2), only screen and (min-width: 1005px) and (-webkit-min-device-pixel-ratio: 1.5), only screen and (min-width: 1005px) and (min-device-pixel-ratio: 1.5), only screen and (min-width: 1005px) and (min-resolution: 1.5dppx) { #forecast { background-image: url(' + assetFQDN + '/static/weather/' + json.code + '@2x.png); -moz-background-size: 115px 115px; -ie-background-size: 115px 115px; -o-background-size: 115px 115px; -webkit-background-size: 115px 115px; background-size: 115px 115px; } }</style>');
+			
+			/*
+			// Below gives me "unexpected call to method or property access" in <IE9.
 			$('<style />')
 				// For this example, we don't want to load images, retina included, at a viewport width < 1005px:
 				.append('@media only screen and (min-width: 1005px) { #forecast { background-repeat: no-repeat; background-image: url(' + assetFQDN + '/static/weather/' + json.code + '.png); } } @media only screen and (min-width: 1005px) and (min--moz-device-pixel-ratio: 1.5), only screen and (min-width: 1005px) and (-moz-min-device-pixel-ratio: 1.5), only screen and (min-width: 1005px) and (-o-min-device-pixel-ratio: 3/2), only screen and (min-width: 1005px) and (-webkit-min-device-pixel-ratio: 1.5), only screen and (min-width: 1005px) and (min-device-pixel-ratio: 1.5), only screen and (min-width: 1005px) and (min-resolution: 1.5dppx) { #forecast { background-image: url(' + assetFQDN + '/static/weather/' + json.code + '@2x.png); -moz-background-size: 115px 115px; -ie-background-size: 115px 115px; -o-background-size: 115px 115px; -webkit-background-size: 115px 115px; background-size: 115px 115px; } }')
 				// Append the styles to the `<head>`; note that the media queries will not work in IE < 8. :(
 				.appendTo('head');
+			*/
 			
 			// Append `html` to widget:
 			$widget.append('<span title="' + json.skies + '"><a href="' + homeFQDN + '/weather/">' + json.temp_f + '&deg;</a></span>');
-				
+			
 		} // successful()
 		
 		//--------------------------------------------------------------------
