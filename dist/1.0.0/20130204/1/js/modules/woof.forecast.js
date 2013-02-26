1f8e1bf967fb4da17b6e6ac96df467c603c4c4b1
Micky Hulse <micky@registerguard.com>
2013-02-25 23:39:58 -0800

    Moving initial build into dist folder.

---
+++ dist/1.0.0/20130204/1/js/modules/woof.forecast.js
@@ -0,0 +1,105 @@
+WOOF.register(function() {
+	
+	/**
+	 * Forecast widget.
+	 * Gets weather data as `jsonp` response from ReSTful API and creates widget on page.
+	 * This JS is specifically tailored to the RG HTML/templates... At some point I may make it more modular.
+	 *
+	 * @dependency jQuery
+	 * @dependency jquery-jsonp
+	 * @pattern Singleton instance/module pattern.
+	 * @see rgne.ws/R5aLJg
+	 */
+	
+	var forecast = (function() {
+		
+		//----------------------------------
+		// Local private variable(s):
+		//----------------------------------
+		
+		var $widget = $('#forecast'),                      // Target `<div>` on template.
+		apiFQDN     = 'http://projects.registerguard.com', // JSON API Fully Qualified Domain Name.
+		assetFQDN   = 'http://projects.registerguard.com', // Image server FQDN.
+		homeFQDN    = 'http://projects.registerguard.com'; // Weather landing page FQDN.
+		
+		//--------------------------------------------------------------------
+		
+		/**
+		 * Called if successful API request has been made.
+		 *
+		 * @param { object } The JSON object returned from the server.
+		 */
+		
+		function successful(json) {
+			
+			// CSS media queries and style info for `<head>` of document:
+			$('<style />')
+				// For this example, we don't want to load images, retina included, at a viewport width < 1005px:
+				.append('@media only screen and (min-width: 1005px) { #forecast { background-repeat: no-repeat; background-image: url(' + assetFQDN + '/static/weather/' + json.code + '.png); } } @media only screen and (min-width: 1005px) and (min--moz-device-pixel-ratio: 1.5), only screen and (min-width: 1005px) and (-moz-min-device-pixel-ratio: 1.5), only screen and (min-width: 1005px) and (-o-min-device-pixel-ratio: 3/2), only screen and (min-width: 1005px) and (-webkit-min-device-pixel-ratio: 1.5), only screen and (min-width: 1005px) and (min-device-pixel-ratio: 1.5), only screen and (min-width: 1005px) and (min-resolution: 1.5dppx) { #forecast { background-image: url(' + assetFQDN + '/static/weather/' + json.code + '@2x.png); -moz-background-size: 115px 115px; -ie-background-size: 115px 115px; -o-background-size: 115px 115px; -webkit-background-size: 115px 115px; background-size: 115px 115px; } }')
+				// Append the styles to the `<head>`; note that the media queries will not work in IE < 8. :(
+				.appendTo('head');
+			
+			// Append `html` to widget:
+			$widget.append('<span title="' + json.skies + '"><a href="' + homeFQDN + '/weather/">' + json.temp_f + '&deg;</a></span>');
+				
+		} // successful()
+		
+		//--------------------------------------------------------------------
+		
+		return {
+			
+			/**
+			 * Initializes module.
+			 *
+			 * @constructor
+			 */
+			
+			init : function() {
+				
+				//----------------------------------
+				// Check for container's existence:
+				//----------------------------------
+				
+				if ($widget.length) {
+					
+					//----------------------------------
+					// Use $.jsonp or $.getJSON?
+					//----------------------------------
+					
+					if ($.jsonp) {
+						
+						/**
+						 * Use @jaubourg's jQuery-JSONP plugin.
+						 *
+						 * @see rgne.ws/OEYxY2
+						 */
+						
+						$.jsonp({
+							url : apiFQDN + '/weather/widget3/?callback=?',
+							cache : true,
+							success : successful
+						});
+						
+					} else {
+						
+						/**
+						 * Use jQuery's $.getJSON.
+						 *
+						 * @see rgne.ws/S7EsQY
+						 */
+						
+						$.getJSON(apiFQDN + '/weather/widget3/?callback=?', successful);
+						
+					}
+					
+				}
+				
+			} // init()
+			
+		};
+		
+	})(); // forecast
+	
+	forecast.init();
+	
+}); // WOOF!
---
+++ dist/1.0.0/20130204/1/js/modules/woof.megas.js
@@ -0,0 +1,29 @@
+WOOF.register(function() {
+	
+	/**
+	 * Navigation setups.
+	 *
+	 * @dependency jQuery
+	 * @dependency jquery-megakrill
+	 * @dependency jquery-megawhale
+	 */
+	
+	var $megawhale = $('#megawhale'),
+	$html          = $('html');
+	
+	$megawhale
+		.megakrill({
+			cloneRemove : 'li > div',
+			cloneId     : false
+		})
+		.megawhale({
+			eventType      : 'hoverIntent',
+			onStartOutside : function() {
+				$html.addClass('mw-outside');
+			},
+			onEndOutside   : function() {
+				$html.removeClass('mw-outside');
+			}
+		});
+	
+}); // WOOF!
