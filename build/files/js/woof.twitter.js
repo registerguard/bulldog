1083203310307f27081eda3c54946ce80c54c5a0
Micky Hulse <mickyhulse@gmail.com>
2013-06-20 16:07:17 -0700

    More Twitter widget madness!
    Stomped a few old IE bugs. Fixed a few things on the Twitter widget
    test page. Updated woof.twitter.js with latest version.

--- build/files/js/woof.twitter.js
+++ build/files/js/woof.twitter.js
@@ -6,10 +6,11 @@ WOOF.register(function() {
 		
 		if ($('.twitter-timeline').length && ( ! $('#' + id).length)) {
 			
-			$('<script>')
-				.appendTo('head')
-				.attr('id', id)
-				.attr('src', '//platform.twitter.com/widgets.js');
+			$('<script>', {
+				'id' : id,
+				'src' : '//platform.twitter.com/widgets.js'
+			})
+				.appendTo('head');
 			
 		}
 		
